f9c091c448a3dcaf725adf6180c8cb137158ccff
lefortnuno <trofelnuno@gmail.com>
2024-10-09 17:56:34 +0100

    Auto Online Env by Trofel

--- hmac/src/contexts/api/axios.js
+++ hmac/src/contexts/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    `http://` +
+    `` +
+    process.env.REACT_APP_SUN_API_HEAD +
     process.env.REACT_APP_SUN_API_IP_ADRESS +
     // process.env.REACT_APP_SUN_API_PORT +
     `/api/`,
